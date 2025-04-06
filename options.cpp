cda882debd1de8bec4a174bf9c582c91317344ab
yeamin <yeaminsardersr@gmail.com>
2025-04-06 09:57:33 +0600

    add -m --inputmap option
    builds the keymap map in options struct

--- options.cpp
+++ options.cpp
@@ -1,4 +1,5 @@
 #include "options.h"
+#include <cstring>
 #include <getopt.h>
 #include <iostream>
 
@@ -6,13 +7,15 @@ const char *help_message =
 R"(Usage: chip8 [OPTIONS] ROM
 Emulate the chip8 ROM
 
-  -d, --debug-level=LEVEL    set debug level to LEVEL. default 1.
-                             0  -> disable
-                             >0 -> show registers
-                             >2 -> show callstack on return
-  -f, --fps=NUM              limits the framerate of emulation to NUM; default 60.
-                             set to 0 for no framerate limit.
-  -h, --help                 show this help
+  -d, --debug-level=LEVEL  set debug level to LEVEL. default 1.
+                           0  -> disable
+                           >0 -> show registers
+                           >2 -> show callstack on return
+  -f, --fps=NUM            limits the framerate of emulation to NUM; default 60.
+                           set to 0 for no framerate limit.
+  -h, --help               show this help
+  -m, --inputmap=MAP       MAP must be exactly 16 characters. default 0123456789abcdef
+                           You can use these characters as key intead of 0x0-0xf
 )";
 
 
@@ -23,6 +26,7 @@ const struct option longopts[] = {
      .val = 'd'},
     {.name = "fps", .has_arg = required_argument, .flag = NULL, .val = 'f'},
     {.name = "help", .has_arg = no_argument, .flag = NULL, .val = 'h'},
+    {.name = "inputmap", .has_arg = required_argument, .flag = NULL, .val = 'm'},
     {} //last element must be all zeros
 };
 
@@ -31,7 +35,7 @@ const struct option longopts[] = {
 void get_options(int argc, char *argv[], options *op) {
 
   while (true) {
-    int o = getopt_long(argc, argv, "hd:f:", longopts, NULL);
+    int o = getopt_long(argc, argv, "hd:f:m:", longopts, NULL);
     if (o == -1)
       break;
     switch (o) {
@@ -50,6 +54,16 @@ void get_options(int argc, char *argv[], options *op) {
         std::cerr << "fps must be an integer";
         exit(1);
       };
+      break;
+    case 'm':
+      if (strlen(optarg) != 16) {
+	std::cerr << "MAP must be exactly 16 characters long";
+	exit(1);
+      }
+      op->keymap.clear();
+      for (int i=0; i < 16; ++i) {
+	op->keymap.emplace(optarg[i],i);
+      }
       break;
     case 'h':
     case '?':
--- options.h
+++ options.h
@@ -1,6 +1,9 @@
+#pragma once
+#include <map>
 typedef struct options {
   int debug_lvl = 1;
   int fps = 60;
   char* filename;
+  std::map<char,char> keymap = {};
 } options;
 void get_options(int argc, char* argv[], options *op);
